ffacd053ca00f07d0d25b305e514dfc9beda7738
Trung Nguyễn <trung9204@gmail.com>
2024-12-28 16:52:34 +0700

    feat: create environment variable

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,16 +1,15 @@
-const MONGODB_URI = "mongodb://localhost:27017";
-const DATABASE_NAME = "balance_management";
+import { env } from "~/config/environment";
 
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient } from "mongodb";
 
 let balanceManagementDBInstance = null;
 
-const mongoClientInstance = new MongoClient(MONGODB_URI);
+const mongoClientInstance = new MongoClient(env.MONGODB_URI);
 
 export const CONNECT_DB = async () => {
     await mongoClientInstance.connect();
 
-    balanceManagementDBInstance = mongoClientInstance.db(DATABASE_NAME);
+    balanceManagementDBInstance = mongoClientInstance.db(env.DATABASE_NAME);
 };
 
 export const CLOSE_DB = async () => {
--- src/server.js
+++ src/server.js
@@ -1,20 +1,18 @@
 import express from "express";
 import { CONNECT_DB, GET_DB, CLOSE_DB } from "~/config/mongodb";
 import exitHook from "async-exit-hook";
+import { env } from "~/config/environment";
 
 const START_SERVER = () => {
     const app = express();
-
-    const hostname = "localhost";
-    const port = 8017;
 
     app.get("/", async (req, res) => {
         console.log(await GET_DB().listCollections().toArray());
         res.end("<h1>Hello World!</h1><hr>");
     });
 
-    app.listen(port, hostname, () => {
-        console.log(`Hello Chun, I am running at http://${hostname}:${port}/`);
+    app.listen(env.APP_PORT, env.APP_HOST, () => {
+        console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`);
     });
 
     exitHook(() => {
