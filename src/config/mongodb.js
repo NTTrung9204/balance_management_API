881ac6037e183fed1580ba33460214f8b1969f2b
Trung Nguyễn <trung9204@gmail.com>
2024-12-28 15:55:15 +0700

    feat: connect to mongodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,1 +1,19 @@
-// This file use to config mongodb
+const MONGODB_URI = "mongodb://localhost:27017";
+const DATABASE_NAME = "balance_management";
+
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+let balanceManagementDBInstance = null;
+
+const mongoClientInstance = new MongoClient(MONGODB_URI);
+
+export const CONNECT_DB = async () => {
+    await mongoClientInstance.connect();
+
+    balanceManagementDBInstance = mongoClientInstance.db(DATABASE_NAME);
+};
+
+export const GET_DB = () => {
+    if (!balanceManagementDBInstance) throw new Error("Must connect to database first!");
+    return balanceManagementDBInstance;
+};
--- src/server.js
+++ src/server.js
@@ -1,16 +1,30 @@
-import express from 'express'
-import { sortHelper } from '~/utils/sorts.js'
+import express from "express";
+import { CONNECT_DB, GET_DB } from "~/config/mongodb";
 
-const app = express()
+const START_SERVER = () => {
+    const app = express();
 
-const hostname = 'localhost'
-const port = 8017
+    const hostname = "localhost";
+    const port = 8017;
 
-app.get('/', (req, res) => {
-  console.log(sortHelper())
-  res.end('<h1>Hello World!</h1><hr>')
-})
+    app.get("/", async (req, res) => {
+        console.log(await GET_DB().listCollections().toArray());
 
-app.listen(port, hostname, () => {
-  console.log(`Hello Chun, I am running at http://${ hostname }:${ port }/`)
-})
+        res.end("<h1>Hello World!</h1><hr>");
+    });
+
+    app.listen(port, hostname, () => {
+        console.log(`Hello Chun, I am running at http://${hostname}:${port}/`);
+    });
+};
+
+(async () => {
+    try {
+        console.log("Connecting to MongoDB Compass...");
+        await CONNECT_DB();
+        console.log("Connected to MongoDB Compass!");
+        START_SERVER();
+    } catch (error) {
+        console.error(error);
+    }
+})();
