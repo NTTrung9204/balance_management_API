8cd65fa7612443b743dfe460145b520e807fb911
Trung Nguyễn <trung9204@gmail.com>
2024-12-31 21:42:17 +0700

    feat: config CORS, allow request from web client to get or post data

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import express from "express";
+import cors from "cors"
 import { CONNECT_DB, GET_DB, CLOSE_DB } from "~/config/mongodb";
 import exitHook from "async-exit-hook";
 import { env } from "~/config/environment";
 import { APIs_V1 } from "~/routes/v1"
-import {errorHandlingMiddleware} from "~/middlewares/errorHandlingMiddleware"
+import {errorHandlingMiddleware} from "~/middlewares/errorHandlingMiddleware";
+import { corsOptions } from "./config/cors";
 
 const START_SERVER = () => {
     const app = express();
+
+    app.use(cors(corsOptions))
 
     app.use(express.json())
 
